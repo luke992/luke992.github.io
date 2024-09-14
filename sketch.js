2cbf31c04cceee1d8c77ef4f258aabffda2ae2e3
Meme-Account <tanish.bengani@gmail.com>
2024-09-13 22:57:17 -0400

    added p5

---
+++ sketch.js
@@ -0,0 +1,59 @@
+let ratio = 16 / 9;
+let wU;
+let hU;
+
+function setup() {
+    if (windowHeight * ratio > windowWidth) {
+        createCanvas(windowWidth, windowWidth / ratio);
+    } else {
+        createCanvas(windowHeight * ratio, windowHeight);
+    }
+
+    wU = width / 16;
+    hU = height / 9;
+}
+
+function draw() {
+    background(255);
+
+    stroke(0);
+    strokeWeight(hU / 9);
+    fill(255);
+
+    // light
+    push();
+    noStroke();
+    fill(255, 255, 0, 50);
+    rect(6 * wU, hU / 2, 6 * wU, hU / 2);
+    pop();
+
+    // walls
+    rect(0, 0, 6 * wU, hU);
+    rect(12 * wU, 0, 4 * wU, hU);
+
+    // door
+    rect(6 * wU, 0, 6 * wU, hU / 2);
+
+    // task page
+    rect(wU / 2, 2 * hU, 5 * wU, 6 * wU);
+
+    // code page
+    rect(6.5 * wU, 1.25 * hU, 5 * wU, 2 * hU);
+
+    // response page
+    rect(6.5 * wU, 3.75 * hU, 5 * wU, 5 * hU);
+
+    // symbol buttons
+    for (let i = 0; i < 3; i++) {
+        rect(12.5 * wU, 2 * hU + i * 1.25 * hU, 1.25 * hU, 1.25 * hU);
+        rect(13.75 * wU, 2 * hU + i * 1.25 * hU, 1.25 * hU, 1.25 * hU);
+    }
+
+    // delete button
+    fill(255, 0, 0, 50);
+    rect(12.5 * wU, 5.75 * hU, 2.5 * hU, 1.25 * hU);
+
+    // submit button
+    fill(0, 255, 0, 50);
+    rect(12.5 * wU, 7 * hU, 2.5 * hU, 1.25 * hU);
+}
